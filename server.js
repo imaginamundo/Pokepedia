ab53b0b2c99082121dbd5735e0f89bdcaffa1432
Diogo Fernandes <diogos.go@gmail.com>
2015-08-11 21:30:30 -0300

    add master and css framework

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
 
+app.engine('handlebars',
+    exphbs({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
+
+// Index
 app.get ('/', function(req, res) {
-    res.send('Hello world!');
+    res.render('index');
 });
 
-app.use('/public', express.static('public'));
+// Search
+app.get ('/search', function(req, res) {
+    res.render('about');
+});
+
+// Favorites
+app.get ('/favorites', function(req, res) {
+    res.render('about');
+});
+
+// Config
+app.get ('/config', function(req, res) {
+    res.render('about');
+});
+
+// Content
+app.use('/content', express.static('content'));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
